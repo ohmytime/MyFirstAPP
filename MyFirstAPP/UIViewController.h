8bd6d7f43dee98b6a2ec54b2671a6e02832c9968
kevin <liqingyu.kevin#gmail>
2015-01-10 20:13:44 +0800

    第一个iOS应用

---
+++ MyFirstAPP/UIViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController.h
+//  MyFirstAPP
+//
+//  Created by kevin on 15/1/3.
+//  Copyright (c) 2015年 kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController : UIViewController
+
+@end
--- MyFirstAPP/ViewController.h
+++ MyFirstAPP/ViewController.h
@@ -7,9 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "UIViewController.h"
+
 
 @interface ViewController : UIViewController
-
 
 @end
 
